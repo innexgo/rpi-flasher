67dd4c90e2c8fe8313b7aaee49fc327cec0b7c3d
pimpale <gpimpale29@gmail.com>
2019-10-07 15:10:49 +0000

    added setup_rpi_flasher

---
+++ setup_rpi_flasher.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+
+# This file to be run on host to set up raspberry pi
+set -e
+
+if [[ $# -ne 2 ]]; then
+    echo "Illegal number of parameters, need 1 drive and 1 hostname of the device"
+    exit 2
+fi
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+
+DRIVE=$1
+
+mkdir -p mnt
+mount "${DRIVE}1" mnt
+touch mnt/ssh
+
+# Copy files into the boot
+cp innexgo-flasher.json mnt/
+cp wpa_supplicant.conf mnt/
+
+umount mnt
+
+# Mount ext4 part on the mount directory
+mount "${DRIVE}2" mnt
+
+# clone to here
+git clone https://github.com/innexgo/rpi-flasher mnt/home/pi/rpi-flasher
+
+# Set the hostname
+echo $2 > mnt/etc/hostname
+sed -i "s/raspberrypi/$2/g"
+
+umount mnt
+
+rmdir mnt

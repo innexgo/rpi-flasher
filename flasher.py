7cacd0ec5e2de71b5872047de8826fe9565b8151
gpi <gpimpale29@gmail.com>
2019-10-08 14:54:01 -0700

    yes

--- flasher.py
+++ flasher.py
@@ -14,14 +14,53 @@ def isPi():
 
 # if raspberry pi
 if isPi():
-    import RPi.GPIO
+    import RPi.GPIO as GPIO
     import mfrc522
 else:
-    print('false')
+    print('not a pi lmao')
+
+# This is the default key for authentication
+rfidKey = [0xFF,0xFF,0xFF,0xFF,0xFF,0xFF]
 
 apiKey = None
 protocol = None
 hostname = None
+
+def beep(hertz, duration):
+
+    # Set up soundPins for buzzer
+    if not soundInitialized:
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(soundPin, GPIO.OUT)
+    soundChannel = GPIO.PWM(soundPin, hertz)
+    soundChannel.start(50.0)
+    time.sleep(duration)
+    soundChannel.stop()
+
+
+def beepUp():
+    beep(1000, 0.1)
+    beep(2000, 0.1)
+
+
+def beepDown():
+    beep(2000, 0.1)
+    beep(1000, 0.1)
+
+def beepFlat():
+    beep(2000, 0.2)
+
+def beepError():
+    beep(1000, 0.1)
+    time.sleep(0.05)
+    beep(1000, 0.1)
+    time.sleep(0.05)
+    beep(1000, 0.1)
+
+def beepNetError():
+    for i in range(0, 4):
+        beep(1000, 0.01)
+        time.sleep(0.05)
 
 def currentMillis():
     return round(1000 * time.time())
@@ -70,25 +109,36 @@ with open('innexgo-flasher.json') as configfile:
     if isPi():
         try:
             reader = mfrc522.MFRC522()
-            print('Please touch card...')
             while True:
-                (detectstatus, tagtype) = reader.MFRC522_Request(reader.PICC_REQIDL)
-                if detectstatus == reader.MI_OK:
-                    (uidstatus, uid) = reader.MFRC522_Anticoll()
-
-                    # TODO add dings
-                    if uidstatus == reader.MI_OK:
-                        # Convert uid to int
-                        cardId = int(bytes(uid).hex(), 16)
-                        print(f'detected card with id {cardId}')
-                        print('input student id')
-                        try:
-                            studentId = int(input())
+                try:
+                    # First grab student id
+                    print('Please enter student ID...')
+                    studentId = int(input())
+                    print('Please touch card...')
+                    (detectstatus, tagtype) = reader.MFRC522_Request(reader.PICC_REQIDL)
+                    if detectstatus == reader.MI_OK:
+                        (uidstatus, uid) = reader.MFRC522_Anticoll()
+                        # TODO add dings
+                        if uidstatus == reader.MI_OK:
+                            # Convert uid to int
+                            cardId = int(bytes(uid).hex(), 16)
+                            print(f'detected card with id {cardId}')
                             associateCard(cardId, studentId)
-                        except ValueError:
-                            print('Not a valid student id. Failed to associate id')
-                        print('Please touch card...')
-                        time.sleep(0.5)
 
+
+                            # Select the scanned tag
+                            MIFAREReader.MFRC522_SelectTag(uid)
+                            # Authenticate us
+                            authStatus = reader.MFRC522_Auth(reader.PICC_AUTHENT1A, 8, rfidKey, uid)
+                            if authStatus == reader.MI_OK:
+                                print('successfully authenticated to card, beginning write');
+                                # Now we must write the student id for the card
+                                reader.MFRC522_Write(1, uid.to_bytes(4, byteorder='big'))
+                                reader.MFRC522_StopCrypto1()
+                            else:
+                                print('failed to authenticate to card! (still usable though)')
+                            time.sleep(0.5)
+                except ValueError:
+                    print('Not a valid student id. Failed to associate id')
         except KeyboardInterrupt:
             RPi.GPIO.cleanup()

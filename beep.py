c67b253afdb772bfd7e2a772a200893fb5d0a9e2
pimpale <gpimpale29@gmail.com>
2019-11-06 16:03:23 +0000

    write id

--- beep.py
+++ beep.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-#######
-# This program would generate PWM on GPIO 40 Pin 26 of P1
-# with 50% Dutycyle at 1kHz
-#######
+
 import RPi.GPIO as GPIO
 from time import sleep
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(40,GPIO.OUT)
-p=GPIO.PWM(40,1190)
+
+GPIO.setmode(GPIO.BCM)
+
+pin = 14
+
+GPIO.setup(pin,GPIO.OUT)
+p = GPIO.PWM(pin,1190)
 p.start(50.0)
+
 input("Press Enter key to Stop 1kHz PWM @ 50% duty cycle")
+
 GPIO.cleanup()
